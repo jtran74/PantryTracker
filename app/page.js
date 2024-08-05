44f318002da10ac94d5b4f036d300adc4512e0ad
Jason Tran <keyotter.jason@gmail.com>
2024-08-04 23:55:49 -0700

    remove some state variables

--- app/page.js
+++ app/page.js
@@ -9,8 +9,7 @@ export default function Home() {
   const [inventory, setInventory] = useState([])
   const [open, setOpen] = useState(false)
   const [itemName, setItemName] = useState('')
-  const [searchQuery, setSearchQuery] = useState('');
-  const [items, setItems] = useState([]);
+
 
   const updateInventory = async () => {
     const snapshot = query(collection(firestore, 'inventory'))
@@ -120,10 +119,6 @@ export default function Home() {
         value = {searchQuery}
         onChange={(e) => setSearchQuery(e.target.view)}
       />
-      
-      {items.map(item => (
-        <div>{item}</div>
-      ))}
   
       <Button variant="contained" onClick={() => {
         handleOpen()
